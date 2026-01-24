8e1ed7c9e7e32fcd1b4811d04a4b37b78735691c
Tim Caussignac <t.caussignac@gmail.com>
2026-01-25 00:04:40 +0100

    Add @vercel/speed-insights for performance tracking integration

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import App from './App';
 
 const rootElement = document.getElementById('root');
@@ -13,5 +14,6 @@ root.render(
   <React.StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 );
